73c987c455a84314d40e5baff86cff802031d7b2
kenty-gaming-ch <minakenty@gmail.com>
2026-06-06 08:55:18 +0900

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,16 +1,17 @@
 const AI_CHARS =
-  "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+"あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
 
 function aiMove() {
-  if (game.step >= 17) return;
-  if (game.turn !== "ai") return;
 
   const ch =
-    AI_CHARS[Math.floor(Math.random() * AI_CHARS.length)];
+    AI_CHARS[
+      Math.floor(Math.random() * AI_CHARS.length)
+    ];
 
   game.phrase += ch;
   game.step++;
 
   render();
+
   nextTurn();
 }
